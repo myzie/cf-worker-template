059d9d354479eba137784a19b4b4777133d3486a
Curtis Myzie <curtis.myzie@gmail.com>
2024-02-07 16:09:40 -0500

    Updates

--- index.ts
+++ index.ts
@@ -1,24 +1,25 @@
-import {Client} from "pg";
+import { Client } from 'pg';
 
 export interface Env {
-    DB: string;
+  DB: string;
 }
 
 export default {
-    async fetch(
-        request: Request,
-        env: Env,
-        ctx: ExecutionContext
-    ): Promise<Response> {
-        const client = new Client(env.DB);
-        await client.connect();
-        const start = Date.now();
-        const result = await client.query('SELECT 1');
-        const end = Date.now();
-        // Close the database connection, but don't block returning the response
-        ctx.waitUntil(client.end());
-        return new Response(JSON.stringify({"query": end-start}), {
-            headers: {'Content-Type': 'application/json'},
-        });
-    },
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    console.log('DB', env.DB);
+    const client = new Client(env.DB);
+    await client.connect();
+    const start = Date.now();
+    const result = await client.query('SELECT 1');
+    const end = Date.now();
+    // Close the database connection, but don't block returning the response
+    ctx.waitUntil(client.end());
+    return new Response(JSON.stringify({ query: end - start, result }), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  },
 };
